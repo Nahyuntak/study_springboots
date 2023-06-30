ebee2f97055e7a265fdd3cb9717e64cd9c257127
Nahyuntak <hyuntak@snu.ac.kr>
2023-06-30 16:35:04 +0900

    first commit

---
+++ src/main/java/com/tak/study_springboots/restapis/HelloWorldService.java
@@ -0,0 +1,31 @@
+package com.tak.study_springboots.restapis;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+
+@Service(value = "")
+public class HelloWorldService {
+    public ArrayList fakeSelect(String currentPage, String perPage) {
+        // "spm_row": 471, "SN": 1, "CMPNM": "로이유통", "RDNMADR": null
+        // "spm_row": 571, "SN": 2, "CMPNM": "의료유통", "RDNMADR": 3
+        ArrayList arrayList = new ArrayList<>();
+        HashMap resultMap = new HashMap<>();
+        resultMap.put("spm_row", 471);
+        resultMap.put("SN", 1);
+        resultMap.put("CMPNM", "로이유통");
+        resultMap.put("RDNMADR", null);
+        arrayList.add(resultMap);
+
+        resultMap = new HashMap<>();
+        resultMap.put("spm_row", 571);
+        resultMap.put("SN", 2);
+        resultMap.put("CMPNM", "의료유통");
+        resultMap.put("RDNMADR", 3);
+        arrayList.add(resultMap);
+
+        return arrayList;
+    }
+
+}
